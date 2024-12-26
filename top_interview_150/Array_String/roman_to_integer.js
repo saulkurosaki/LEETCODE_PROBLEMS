6f57d41bed9e5369d4ebf9501d1a368c68b42563
saulkurosaki <saulkurosaki13@gmail.com>
2024-12-26 12:27:54 -0600

    feat(top_interview_150): Complete the 13-Roman to Integer. leetcode problem

--- top_interview_150/Array_String/roman_to_integer.js
+++ top_interview_150/Array_String/roman_to_integer.js
@@ -40,3 +40,53 @@
 // 1 <= s.length <= 15
 // s contains only the characters ('I', 'V', 'X', 'L', 'C', 'D', 'M').
 // It is guaranteed that s is a valid roman numeral in the range [1, 3999].
+
+function romanToInt(s) {
+  const romanToInt = {
+    I: 1,
+    V: 5,
+    X: 10,
+    L: 50,
+    C: 100,
+    D: 500,
+    M: 1000,
+  };
+
+  let result = 0;
+  const n = s.length;
+
+  for (let i = 0; i < n - 1; i++) {
+    if (romanToInt[s[i]] < romanToInt[s[i + 1]]) {
+      result -= romanToInt[s[i]];
+    } else {
+      result += romanToInt[s[i]];
+    }
+  }
+
+  result += romanToInt[s[n - 1]];
+
+  return result;
+}
+
+//* Algoritmo
+
+// 	1.	Diccionario de valores romanos:
+//  Crear un diccionario que asocie cada símbolo romano con su valor correspondiente:
+//    roman_to_int = {
+//        'I': 1, 'V': 5, 'X': 10, 'L': 50,
+//        'C': 100, 'D': 500, 'M': 1000
+//    }
+//  2.	Recorrido de la cadena:
+// 	•	Inicializar un acumulador result en 0.
+// 	•	Recorrer la cadena s desde el primer carácter hasta el penúltimo.
+// 	•	Para cada carácter, verificar si su valor es menor que el siguiente:
+// 	•	Si es menor, se resta el valor del acumulador.
+// 	•	Si no, se suma.
+// 	•	Sumar el valor del último carácter, ya que no tiene un carácter siguiente para comparar.
+// 	3.	Devolver el resultado:
+// 	•	El acumulador result contendrá el valor entero equivalente al número romano.
+
+//* Complejidad
+
+// 	•	Tiempo:  O(n) , donde  n  es la longitud de la cadena s. Se recorre la cadena una sola vez.
+// 	•	Espacio:  O(1) , ya que no se utiliza espacio adicional proporcional al tamaño de la entrada.
